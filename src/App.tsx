f5bac12a06489edfd21da726f9cc404151b0184d
LauCaKeo <phuc.cao@mia.vn>
2025-03-30 06:16:43 +0700

    ✨ Thêm logic chuyển view sang OrderStaffAllocation khi bấm 'Quản lý đơn hàng'

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import OrderStaffAllocation from "./components/OrderStaffAllocation";
-import WarehouseDashboard from "./components/enhanced-warehouse-dashboard";
-import WarehouseRealTimeDashboard from "./components/Real-Time-Dashboard";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <WarehouseDashboard />
       <OrderStaffAllocation />
-      <WarehouseRealTimeDashboard />
     </div>
   );
 }
